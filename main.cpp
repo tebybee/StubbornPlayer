b2723f71d5ae21e926d7d70ff4a3aacb3251fc0c
tebybee <tbybee@mail.csuchico.edu>
2018-03-01 11:38:15 -0800

    plays the secret door game as many times as requested and only chooses door c twice. Counts how many times the user wins and reports it at the end of the program.

--- main.cpp
+++ main.cpp
@@ -6,6 +6,20 @@ using namespace std;
 int main()
 {
   SecretDoor game;
-    
+  int input;
+  int wins;
+  cout<<"How many times would you like to play?\n";
+  cin>>input;
+  for(int x=0; x<input; x++)
+  {
+   game.newGame();
+   game.guessDoorC();
+   game.guessDoorC();
+   if(game.isWinner()== true)
+   {
+     wins++;
+   }
+  }
+   cout<<"You've won "<<wins<<" times\n";
   return 0;
 }
